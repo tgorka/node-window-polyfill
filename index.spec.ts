32713455919b7f71dbdf2a6a5b43474aadb7f361
tgorka <tomasz.gorka@carmoov.io>
2018-04-23 15:22:05 +0200

    implement window object

--- index.spec.ts
+++ index.spec.ts
@@ -8,31 +8,37 @@ const debug = _debug("nodeWindowPolyfill-test");
 describe("default polyfill Test", () => {
 
     beforeAll(async () => {
+        (<any>global).window = undefined; // make sure it's not defined
+        (<any>global).WebSocket = undefined;
         nodeWindowPolyfill.register();
-        debug(`after polyfill is called`);
+        debug(`after default polyfill is called. ${typeof window}`);
     });
 
     afterAll(async () => {
-        global.window = undefined;
+        (<any>global).window = undefined;
     });
 
-    it("window should not throw an error", () => expect(window).not.toThrowError());
-    it("window should be instance of an object", () => expect(window).toBeInstanceOf({}));
-    it("window.ws should be instance of an object", () => expect(window.ws).toBeInstanceOf({}));
+    it("window should not throw an error", () => expect(() => {window.isNodeJS}).not.toThrowError());
+    it("window should be defined", () => expect(window).toBeDefined());
+    it("window.isNodeJS is true", () => expect(window.isNodeJS).toBe(true));
+    it("window.WebSocket should be instance of an object", () => expect(window.WebSocket).toBeDefined());
 });
 
 describe("non 'ws' polyfill Test", () => {
 
     beforeAll(async () => {
+        (<any>global).window = undefined; // make sure it's not defined
+        (<any>global).WebSocket = undefined;
         nodeWindowPolyfill.register(false);
-        debug(`after polyfill is called`);
+        debug(`after non 'ws' polyfill is called.`);
     });
 
     afterAll(async () => {
-        global.window = undefined;
+        (<any>global).window = undefined;
     });
 
-    it("window should not throw an error", () => expect(window).not.toThrowError());
-    it("window should be instance of an object", () => expect(window).toBeInstanceOf({}));
-    it("window.ws should be undefined", () => expect(window.ws).toBeUndefined());
+    it("window should not throw an error", () => expect(() => {window.isNodeJS}).not.toThrowError());
+    it("window should be defined", () => expect(window).toBeDefined());
+    it("window.isNodeJS is true", () => expect(window.isNodeJS).toBe(true));
+    it("window.WebSocket should be undefined", () => expect(window.WebSocket).not.toBeDefined());
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,53 @@
+const globalObject = <any>global;
+
+export const registerWebSocket = (): void => {
+    globalObject.WebSocket = globalObject.WebSocket || require("ws");
+};
+
+export const registerWindowProperties = (): void => {
+    globalObject.window.setTimeout = globalObject.setTimeout || setTimeout;
+    globalObject.window.clearTimeout = globalObject.setTimeout || clearTimeout;
+    globalObject.window.WebSocket = globalObject.WebSocket;
+    globalObject.window.ArrayBuffer = globalObject.ArrayBuffer;
+    globalObject.window.addEventListener = globalObject.addEventListener || function () { };
+    globalObject.window.navigator = globalObject.navigator || { onLine: true };
+    globalObject.window.isNodeJS = globalObject.isNodeJS || true;
+};
+
+export const registerWindow = (): void => {
+    globalObject.window = globalObject.window || {};
+    registerWindowProperties();
+};
+
+export const registerLocalStorage = (): void => {
+    globalObject.localStorage = globalObject.localStorage || {
+        store: {},
+        getItem: function (key) {
+            return this.store[key]
+        },
+        setItem: function (key, value) {
+            this.store[key] = value
+        },
+        removeItem: function (key) {
+            delete this.store[key]
+        }
+    };
+};
+
 /**
  * The function to polyfill with adding global.window.
  * So you can use window.{...} later.
  * REMEMBER: the import needs to be declared before imports that use window object.
+ * INFO: It's save polyfill. It's mean if there is global.window it will not be changed.
+ * INFO: Local storage polyfill will use inMemory RAM storage.
  * @param doPolyfillWebSockets and import ws object. By default true.
  */
 export const register = (doPolyfillWebSockets: boolean = true): void => {
-    // TODO: implement me!
-    throw new Error("not implememnted");
+    if (doPolyfillWebSockets) {
+        registerWebSocket();
+    }
+    registerWindow();
+    registerLocalStorage();
 };
 
 export default { register: register };
