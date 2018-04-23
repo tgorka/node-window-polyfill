dc2fac5c1d059c9eafa1096cbc33500f7ae24bba
tgorka <tomasz.gorka@carmoov.io>
2018-04-23 13:51:23 +0200

    declare the functions to use

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * The function to polyfill with adding global.window.
+ * So you can use window.{...} later.
+ * REMEMBER: the import needs to be declared before imports that use window object.
+ * @param doPolyfillWebSockets and import ws object. By default true.
+ */
+export const register = (doPolyfillWebSockets: boolean = true): void => {
+    // TODO: implement me!
+    throw new Error("not implememnted");
+};
+
+export default register;
